2957337e4f9c7a6f9b13fb86361aa3d534c6f039
Owen Winzeler <littlehouse2104@gmail.com>
2022-03-28 18:10:05 -0700

    fixed github pages

--- src/shaders/fragment.glsl
+++
@@ -1,8 +0,0 @@
-varying vec2 vCoordinates; 
-uniform sampler2D imageGraphic; 
-  
-void main(){
-  vec2 myUV = vec2(vCoordinates.x/512., vCoordinates.y/512.);
-  vec4 image = texture2D(imageGraphic, myUV);
-  gl_FragColor = image;
-}
